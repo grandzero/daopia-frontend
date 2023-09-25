d0076f61020d98cba4ad9f1904382b450625cff1
Bayram Utku Uzunlar <38724739+grandzero@users.noreply.github.com>
2023-09-25 18:21:52 +0300

    inital commit, use template from nexth

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import { Text } from '@chakra-ui/react'
+import { Head } from 'components/layout/Head'
+import { HeadingComponent } from 'components/layout/HeadingComponent'
+import { LinkComponent } from 'components/layout/LinkComponent'
+import { useAccount, useSignMessage } from 'wagmi'
+export default function Home() {
+  const { isConnected } = useAccount()
+  return (
+    <>
+      <Head />
+
+      <main>
+        <HeadingComponent as="h2">Next.js + Ethereum starter kit</HeadingComponent>
+        {isConnected ? <Text>Quickly ship Web3 Apps ⚡</Text> : <Text>Connect your wallet</Text>}
+        <Text py={4}>
+          <LinkComponent href="examples">View examples</LinkComponent> to bootstrap development.
+        </Text>
+      </main>
+    </>
+  )
+}
