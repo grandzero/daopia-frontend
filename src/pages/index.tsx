097b1be1fc867427155d9986347dd1f2638d57cc
Bayram Utku Uzunlar <38724739+grandzero@users.noreply.github.com>
2023-09-25 22:45:40 +0300

    conditional rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,20 +11,19 @@ export default function Home() {
   const { isConnected } = useAccount()
   return (
     <>
-      {/* <Head /> */}
-
-      <MainSection />
-      <Box borderBottom="2px solid" borderColor="gray.200" />
-      <FeaturesSection />
-      <Box borderBottom="2px solid" borderColor="gray.200" />
-      <DaoHowItWorksSection />
-      <Box borderBottom="2px solid" borderColor="gray.200" />
-      <UserHowItWorksSection />
-      {/* <HeadingComponent as="h2">Next.js + Ethereum starter kit</HeadingComponent>
-        {isConnected ? <Text>Quickly ship Web3 Apps ⚡</Text> : <Text>Connect your wallet</Text>}
-        <Text py={4}>
-          <LinkComponent href="examples">View examples</LinkComponent> to bootstrap development.
-        </Text> */}
+      {!isConnected ? (
+        <>
+          <MainSection />
+          <Box borderBottom="2px solid" borderColor="gray.200" />
+          <FeaturesSection />
+          <Box borderBottom="2px solid" borderColor="gray.200" />
+          <DaoHowItWorksSection />
+          <Box borderBottom="2px solid" borderColor="gray.200" />
+          <UserHowItWorksSection />
+        </>
+      ) : (
+        <MainSection />
+      )}
     </>
   )
 }
