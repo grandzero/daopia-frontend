8cd07531580a94cd215c65aa55dc8b81725bbe5d
Bayram Utku Uzunlar <38724739+grandzero@users.noreply.github.com>
2023-09-26 01:00:57 +0300

    add fetch daos array

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,7 +1,8 @@
-import { useNetwork, useContractRead } from 'wagmi'
+import { useContractRead } from 'wagmi'
 import { Box, Grid, Image, Text, Button, useColorMode, VStack, Heading, Center, Container } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
-
+import { useEffect, useState } from 'react'
+import daopiaABI from '../assets/abis/daopiaABI'
 const daos = [
   {
     id: 1,
@@ -38,9 +39,22 @@ const daos = [
 
 const Dashboard = () => {
   const { colorMode } = useColorMode()
+  const [daoList, setDaoList] = useState<any>([])
   const router = useRouter()
   const textColor = { light: 'gray.700', dark: 'gray.200' }
   const bgColor = { light: 'gray.50', dark: 'gray.900' }
+  const contractRead = useContractRead({
+    address: `0x${process.env.NEXT_PUBLIC_DAOPIA_CONTRACT_ADDRESS}`,
+    abi: daopiaABI.abi,
+    functionName: 'getDaoList',
+    args: [],
+  })
+  useEffect(() => {
+    ;(async () => {
+      let result = await contractRead.refetch()
+      result?.data && setDaoList(result.data)
+    })()
+  }, [])
 
   return (
     <VStack minHeight="100vh" spacing={0} align="stretch">
@@ -50,13 +64,13 @@ const Dashboard = () => {
             Dashboard
           </Heading>
           <Grid templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)', lg: 'repeat(3, 1fr)' }} gap={6}>
-            {daos.map((dao) => (
-              <Center key={dao.id} p={6} boxShadow="lg" borderRadius="md" bg={colorMode === 'light' ? 'white' : 'gray.700'}>
+            {daoList.map((dao: any) => (
+              <Center key={dao.dao} p={6} boxShadow="lg" borderRadius="md" bg={colorMode === 'light' ? 'white' : 'gray.700'}>
                 <VStack spacing={4}>
-                  <Image src={dao.logo} alt={dao.name} boxSize="100px" objectFit="cover" />
+                  <Image src={dao.logoUrl} alt={dao.name} boxSize="100px" objectFit="cover" />
                   <Heading size="md">{dao.name}</Heading>
                   <Text>{dao.description}</Text>
-                  <Button colorScheme="blue" variant="outline" onClick={() => router.push(`/dao/details/${dao.id}`)}>
+                  <Button colorScheme="blue" variant="outline" onClick={() => router.push(`/dao/details/${dao.dao}`)}>
                     Show Details
                   </Button>
                 </VStack>
